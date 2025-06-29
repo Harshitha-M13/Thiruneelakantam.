86b7bed5efad10b8a9bd6902e1b5477569712fe6
Harshitha_Mahadev <apple@apples-MacBook-Pro.local>
2025-06-29 15:23:30 +0530

    PS insta Path parameter being commited

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express") ;
+const instaData = require("./dataPs.json") ;
+const app = express() ;
+
+const port = 333 ;
+app.set("view engine" , "ejs") ;
+app.use(express.static("public")) ;
+
+app.use( (req , res , next) => {
+    console.log("ParvatiammaShivappa , request received...") ;
+    next() ;
+}) ;
+
+app.get("/" , (req , res) => {
+    res.send("<h1>ShivappaParvatiamma says this is the root route</h1>") ;
+}) ;
+app.get("/www.instagram.com/:username" , (req , res) => {
+    console.log(req.params) ;
+    let { username } = req.params ;
+    console.log(req.query) ;
+    let { q , bgColor , color } = req.query ;
+    let data = instaData[username] ;
+    res.render("home.ejs" , { instaData : data }) ;
+}) ;
+app.listen(port , () => {
+    console.log(`ParvatiammaShivappa , server listening`) ;
+})
