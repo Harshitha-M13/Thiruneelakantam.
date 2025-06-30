8c61052729d8b518c2865713220908a37834093b
Harshitha MahadevMahadevi <shivsaktiharshitha13@gmail.com>
2025-06-30 16:02:22 +0530

    ParvatiammaShivappa insta path-parameter

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-let btns = document.querySelectorAll("type = 'button'") ;
+let btns = document.querySelectorAll("button") ;
 
 for(let i = 0 ; i <= btns.length ; i++) {
     console.log("ParvatiammaShivappa , button was clicked") ;
